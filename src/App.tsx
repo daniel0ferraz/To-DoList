cb1e5d07a066ad77d52bfa244631272f0dd54d42
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-10-25 16:17:37 -0300

    finsh project

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,44 @@ import React, { useState } from "react";
 import { Item } from "./types/item";
 import * as C from "./app.styles";
 import ListItem from "./components/ListItem/index";
+import AddArea from "./components/AddArea/index";
 
 export default function App() {
-	const [list, SetList] = useState<Item[]>([
-		{ id: 1, name: "Comprar Fonte 500W", done: false },
-		{ id: 2, name: "Comprar Bolo na padaria", done: false },
+	const [list, setList] = useState<Item[]>([
+		{ id: 1, name: "Comprar o pão na padaria", done: false },
+		{ id: 2, name: "Comprar um bolo na padaria", done: true },
 	]);
+
+	const handleAddTask = (taskName: string) => {
+		let newList = [...list];
+		newList.push({
+			id: list.length + 1,
+			name: taskName,
+			done: false,
+		});
+		setList(newList);
+	};
+
+	const handleTaskChange = (id: Number, done: boolean) => {
+		let newList = [...list];
+		for (let i in newList) {
+			if (newList[i].id === id) {
+				newList[i].done = done;
+			}
+		}
+		setList(newList);
+	};
+
 	return (
 		<>
 			<C.Container>
 				<C.Area>
 					<C.Header>Lista de Tarefas</C.Header>
 
-					{/*ÁREA DE ADICIONAR NOVA TAREFA */}
+					<AddArea onEnter={handleAddTask} />
 
 					{list.map((item, index) => (
-						<ListItem key={index} item={item} />
+						<ListItem key={index} item={item} onChange={handleTaskChange} />
 					))}
 				</C.Area>
 			</C.Container>
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,12 +3,20 @@ import { Item } from "../../types/item";
 
 type Props = {
 	item: Item;
+	onChange: (id: Number, done: boolean) => void;
 };
 
-export default function ListItem({ item }: Props) {
+export default function ListItem({ item, onChange }: Props) {
 	return (
 		<>
-			<C.Container>Olá, Pivetão...</C.Container>
+			<C.Container done={item.done}>
+				<input
+					type="checkbox"
+					checked={item.done}
+					onChange={(e) => onChange(item.id, e.target.checked)}
+				/>
+				<label>{item.name}</label>
+			</C.Container>
 		</>
 	);
 }
